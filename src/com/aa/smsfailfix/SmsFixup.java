b002883bbedcb42c3f7e6566846aea1c1773a6af
Andrea Arcangeli <andrea@cpushare.com>
2012-02-27 21:04:36 +0100

    catch SQLiteException error code 5: database is locked
    In the Market developer console I noticed one exception triggered
    (supposedly causing a force close upon sms reception). So this change
    tries to catch the exception and it retry a few times.
    
    I can't reproduce on the emulator, maybe some 3rd party app keeps the
    db open for too long.
    
    java.lang.RuntimeException: Unable to start receiver com.aa.smsfailfix.SmsReceiver: android.database.sqlite.SQLiteException: error code 5: database is locked
    at android.app.ActivityThread.handleReceiver(ActivityThread.java:2821)
    at android.app.ActivityThread.access$3200(ActivityThread.java:125)
    at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2083)
    at android.os.Handler.dispatchMessage(Handler.java:99)
    at android.os.Looper.loop(Looper.java:123)
    at android.app.ActivityThread.main(ActivityThread.java:4627)
    at java.lang.reflect.Method.invokeNative(Native Method)
    at java.lang.reflect.Method.invoke(Method.java:521)
    at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:858)
    at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:616)
    at dalvik.system.NativeStart.main(Native Method)
    Caused by: android.database.sqlite.SQLiteException: error code 5: database is locked
    at android.database.DatabaseUtils.readExceptionFromParcel(DatabaseUtils.java:158)
    at android.database.DatabaseUtils.readExceptionFromParcel(DatabaseUtils.java:114)
    at android.content.ContentProviderProxy.update(ContentProviderNative.java:495)
    at android.content.ContentResolver.update(ContentResolver.java:707)
    at com.aa.smsfailfix.SmsFixup.smsFixup(SmsFixup.java:24)
    at com.aa.smsfailfix.SmsReceiver.onReceive(SmsReceiver.java:19)
    at android.app.ActivityThread.handleReceiver(ActivityThread.java:2810)
    ... 10 more

--- src/com/aa/smsfailfix/SmsFixup.java
+++ src/com/aa/smsfailfix/SmsFixup.java
@@ -8,24 +8,43 @@
 package com.aa.smsfailfix;
 
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.util.Log;
 import android.net.Uri;
+import android.os.SystemClock;
+import android.database.sqlite.SQLiteException;
 
 public class SmsFixup {
 	private static final String reply_path_present = "reply_path_present";
 	private final static String TAG = "SmsFailFix";
 
 	static int smsFixup(Context context) {
-		int nr;
+		int nr = 0;
 		ContentValues values = new ContentValues(1);
 		Log.d(TAG, "start fixup");
 		values.put(reply_path_present, 0);
-		nr = context.getContentResolver().update(Uri.parse("content://sms/inbox"),
-							 values,
-							 reply_path_present + "!=" + "0",
-							 null);
-		Log.d(TAG, "end fixup, updated " + nr + " SMS");
+
+		ContentResolver contentResolver = context.getContentResolver();
+		int retries = 3;
+		while (retries > 0) {
+			try {
+				//throw new SQLiteException("failed");
+				nr = contentResolver.update(Uri.parse("content://sms/inbox"),
+							    values,
+							    reply_path_present + "!=" + "0",
+							    null);
+				retries = -1;
+			} catch (SQLiteException e) {
+				Log.d(TAG, "SQLiteException, retrying...");
+				SystemClock.sleep(1000);
+				retries--;
+			}
+		}
+		if (retries == -1)
+			Log.d(TAG, "end fixup, updated " + nr + " SMS");
+		else
+			Log.d(TAG, "end fixup, failed with SQLiteException");
 		return nr;
 	}
 }
